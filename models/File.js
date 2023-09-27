a81d20e2be50a468d869988999fce0c0870652f1
Nikhil <solankinikhil74@gmail.com>
2023-09-27 10:21:27 +0530

    Now the response can be sended to cloudinary and backend server but also emailing to the given email id

--- models/File.js
+++ models/File.js
@@ -1,5 +1,8 @@
 const mongoose =  require("mongoose");
+const nodemailer = require("nodemailer")
 
+
+require("dotenv").config();
 const fileSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -16,6 +19,37 @@ const fileSchema = new mongoose.Schema({
     }
 
 })
+
+fileSchema.post("save" , async function(doc){
+    try{
+        console.log("doc" , doc);
+
+        let transporter = nodemailer.createTransport({
+            host:process.env.MAIL_HOST,
+            auth:{
+                 user:process.env.MAIL_USER,
+                 pass:process.env.MAIL_PASS,
+            }
+        })
+        let info =await transporter.sendMail({
+            from:`Head of the Department-Nikhil Solanki`,
+            to:doc.email,
+            subject:"New file uploaded of cloudinary",
+            html:`<h1>HELLO BROTHER </h1><p>FILE UPLOADED , Sir</p>`
+        })
+
+        console.log("info bro" , info)
+
+    }catch(error){
+       console.log(error);
+        res.status(401).json({
+            success:false,
+            message:'there is an error occured from your mistake right your'
+        })
+    }
+})
+
+
 
 const File = mongoose.model("File" , fileSchema);
 module.exports = File;
